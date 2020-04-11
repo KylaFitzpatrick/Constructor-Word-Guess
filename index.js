c417a9c40d251fe36cd7c464fb51daa5d45cb59f
fitzpaky <kfitzpatrick44@gmail.com>
2020-04-11 07:49:46 -0700

    adding new word

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const inquirer = require("inquirer");
 const Word = require("./Word")
 
 function Game() {
-    this.requireNewWord = false;
-    this.wins = 0;
-    this.loses = 0;
+    this.newWord =false;
     this.guesses = 10;
-    this.incorrectLetters = [];
-    this.correctLetters = [];
+    // this.incorrectLetters = [];
+    // this.correctLetters = [];
     this.letterArray = "abcdefghijklmnopqrstuvwxyz";
-    this.guess = "";
+    
 
     // this.randomWords = ["united states", "austrailia", "ireland", "paris", "spain", "portugal", "costa rica"];
     this.randomWords = ["ireland", "paris"]
@@ -25,73 +23,86 @@ function Game() {
     }
 
     this.reset = function () {
-        this.wins = 0;
-        this.loses = 0;
+        // this.wins = 0;
+        // this.loses = 0;
         this.guesses = 10;
         this.wordToGuess = new Word(this.pickRandomWord());
         var game = this;
         guess(this.wordToGuess, game);
 
     }
+    this.newGame = function (){
+        this.guesses = 10;
+        this.wordToGuess = new Word(this.pickRandomWord());
+        var game = this;
+        guess(this.wordToGuess, game); 
+    }
 }
 
 
 function guess(wordToDisplay, game) {
     console.log(wordToDisplay.displayWord());
-   
-    //new word if true
-    if (game.requireNewWord) {
-
-        game.randomWords;
-        game.pickRandomWord();
-        game.wordToGuess;
-
-        requireNewWord = false;
-    }
 
     var question = {
         name: "guess",
         type: "string",
         message: "Guess a letter!"
     }
-    if (game.requireNewWord === false) {
+    if (game.newWord === false) {
         inquirer.prompt([question])
             .then(function (answer) {
                 var letter = answer.guess;
                 wordToDisplay.checkWord(letter);
-                wordToDisplay.haveWeWon();
+                wordToDisplay.haveWon();
                 if (!game.letterArray.includes(letter) || letter.length > 1) {
-                    console.log("\nPlease try again!\n");
-                    guess();
-                }
-                // if(Word.checkWord !== letter){
-                //     // game.guesses--;
-                //     console.log(`You have ${game.guesses} guesses remaining!`)
-                // }
+                    console.log(`Please try again!`);
+                    game.reset();
+                    if(game.guesses > 0 && wordToDisplay.checkWord(letter)){
+                        console.log(`You have ${game.guesses} remaining!`)
+                        game.guesses--;
+                        if(guesses === 0){
+                            console.log(`You lost!`)
+                            game.newWord = true;
+                        }
+                        
+                    }
+                
+                    
+                        
+                    
+                    
+                }else{
+               
                 if (!wordToDisplay.isWon) {
                     guess(wordToDisplay, game);
+                    // if(letter !== )
 
+                }else {
+                    console.log(`Correct! Next word!`)
+                    game.reset()
+                   
+    
                 }
-                if (!wordToDisplay.isWon && wordToDisplay.isLose) {
-                    game.loses++;
-                }
-                if (game.guesses > 0) {
-                    guess()
-                }
-                else {
-                    game.loses--;
-                    console.log(`You have lost ${game.loses}!`)
-                    resetGame()
-                }
+                
+                // if (game.guesses > 0) {
+                //     game.reset()
+                // }
+                // else {
+                    
+                //     console.log(`You lost!`)
+                //     game.newWord = true;
+                // }
+            }
             })
-
-    } else {
-        game.wins++;
-        console.log(`You have won ${game.wins} times!`)
-        resetGame()
+        }else{
+            
+            resetGame()
+      
+        
     }
-
 }
+
+
 
 function resetGame() {
     var question = {
@@ -112,3 +123,8 @@ function resetGame() {
 }
 
 new Game().init();
+
+ // if(Word.checkWord !== letter){
+                //     // game.guesses--;
+                //     console.log(`You have ${game.guesses} guesses remaining!`)
+                // }
